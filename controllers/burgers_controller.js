3f6a5a453dc38eba95f13928f56aba43d506f215
mcarreon <carryon2095@gmail.com>
2018-09-12 22:18:07 -0700

    added bootstrap for stylish burgers, integrated burger_db into handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,10 +4,27 @@ var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
     burger.showAllBurger(function(data) {
+        var availableBurgers = [];
+        var oldBurgers = [];
+        
+        data.forEach(function (e) {
+            if (e.devoured == false) {
+                availableBurgers.push(e);
+            }
+            else {
+                oldBurgers.push(e);
+            }
+        });
+        
+        console.log(availableBurgers);
+        console.log(oldBurgers);
+
         var hbsObject = {
-            burgers: data
+            allBurgers: data,
+            availableBurgers: availableBurgers,
+            oldBurgers: oldBurgers
         };        
-        console.log(hbsObject);
+        //console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
